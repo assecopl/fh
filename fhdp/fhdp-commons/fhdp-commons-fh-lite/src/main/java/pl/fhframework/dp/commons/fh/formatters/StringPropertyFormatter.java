58f3d9f723635610a3f924f865b08088d091ad73
Jacek Borowiec <jacek.borowiec@gmail.com>
2022-01-10 16:26:28 +0100

    [feature](NO_TASK): FHDP module - fixes

--- fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/formatters/StringPropertyFormatter.java
+++ fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/formatters/StringPropertyFormatter.java
@@ -8,7 +8,9 @@ import pl.fhframework.core.i18n.MessageService;
 import pl.fhframework.format.FhFormatter;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -36,12 +38,27 @@ public class StringPropertyFormatter implements Formatter<String> {
                 msg = messageService.getAllBundles().getMessage(key.replace("$.", ""));
             } else {
                 String[] params = Arrays.copyOfRange(elements, 1, elements.length);
-                msg = messageService.getAllBundles().getMessage(elements[0].replace("$.", ""),
-                        params, locale, "key");
+                List<String> finalParams = new ArrayList<>();
+                for(String p: params) {
+                    finalParams.add(resolve(p, locale));
+                }
+                String msgKey = elements[0].replace("$.", "");
+                msg = messageService.getAllBundles().getMessage(msgKey,
+                        finalParams.toArray(new String[0]), locale, "?:" + msgKey);
             }
             return msg;
         } else {
             return LngDescriptionUtil.getDescription(key, SessionManager.getUserSession().getLanguage().toLanguageTag());
         }
     }
+
+    private String resolve(String p, Locale locale) {
+        if(p.startsWith("$.")) {
+            String pKey = p.replace("$.", "");
+            return messageService.getAllBundles().getMessage(pKey,null, locale, "?:" + pKey);
+        } else {
+            return p;
+        }
+    }
+
 }
