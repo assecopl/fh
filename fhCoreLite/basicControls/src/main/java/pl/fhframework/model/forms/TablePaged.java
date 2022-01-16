8a686e3f313ae4077be7cfdeb4d495998b0ef1c4
Paweł Domański <pawel.domanski@asseco.pl>
2022-01-16 09:21:53 +0100

    [fix](NO_TASK): TablePaged selection

--- fhCoreLite/basicControls/src/main/java/pl/fhframework/model/forms/TablePaged.java
+++ fhCoreLite/basicControls/src/main/java/pl/fhframework/model/forms/TablePaged.java
@@ -344,7 +344,7 @@ public class TablePaged extends Table {
             }
         }
 
-        this.selectedRowsNumbers = getSelectedRowNumberBasedOnBinding(currentPage.getContent(), this.multiselect);
+        this.selectedRowsNumbers = getSelectedRowNumberBasedOnBinding(mainCollection, this.multiselect);
         elementChange.addChange(SELECTED_ROW_NUMBER, this.selectedRowsNumbers);
 
         this.language = LanguageResolver.languageChanges(getForm().getAbstractUseCase().getUserSession(), this.language, elementChange);
