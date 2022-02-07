a0079a0ca594f38faf3149f51a5289d179cf05f1
Gabriel Kurzac <gabriel.kurzac@asseco.pl>
2022-02-07 14:01:39 +0100

    Bug fixes for emergency sesion removal after merge

--- fhCoreLite/coreLite/src/main/java/pl/fhframework/core/session/UserSessionRepository.java
+++ fhCoreLite/coreLite/src/main/java/pl/fhframework/core/session/UserSessionRepository.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 import pl.fhframework.UserSession;
@@ -48,6 +49,15 @@ public class UserSessionRepository implements HttpSessionListener, ApplicationLi
     private String sessionInfoProtocol;
     @Value("${fh.ws.closed.inactive_session_max_time:5}")
     private int sustainTimeOutMinutes;
+
+    @Value("${fh.session.emergency_removal_unused_sessions:true}")
+    private boolean emergencyRemovalUnusedSessions;
+
+    /**
+     * The maximum amount of time an unused session can survive - devault value = 12 houres (43200 seconds)
+     */
+    @Value("${fh.session.emergency_removal_time_unused_session:43200}")
+    private int emergencyRemovalTimeUnusedSessionInSeconds;
 
     private String nodeUrl;
 
@@ -225,6 +235,62 @@ public class UserSessionRepository implements HttpSessionListener, ApplicationLi
 
     public Set<UserSession> getAllUserSessions(){
         return new HashSet<>(userSessionsByFhId.values());
+    }
+
+
+    @Scheduled(fixedDelay = 60000)
+    private void cleanupLeakedSessions(){
+        //Calculate collections of session keys (http session id) of sessions to remove
+        Set<String> sessionKeysToRemove =  getKeysToRemove(userSessionsByFhId.entrySet());
+        //Removing sessions in convinient way
+        emergencySessionRemoval(sessionKeysToRemove);
+
+        //Calculate leaked sessions in userSessionsByConversationId
+        sessionKeysToRemove = getKeysToRemove(userSessionsByConversationId.entrySet());
+        unkomonEmergencySessionRemovalInUserSessionsByConversationId(sessionKeysToRemove);
+    }
+
+    private void emergencySessionRemoval(Set<String> sessionKeysToRemove) {
+        sessionKeysToRemove.forEach(key -> {
+            UserSession session = userSessionsByFhId.get(key);
+            FhLogger.warn(UserSessionRepository.class, "Emergency removal of unnecessary session {}, {}", key, getUserLogin(session) );
+            removeUserSession(session);
+        });
+    }
+
+    private void unkomonEmergencySessionRemovalInUserSessionsByConversationId(Set<String> sessionKeysToRemove) {
+        if (sessionKeysToRemove.size()>0){
+            FhLogger.error("Lost sessions in userSessionsByConversationId!!!");
+            sessionKeysToRemove.forEach(key -> {
+                UserSession session = userSessionsByConversationId.get(key);
+                FhLogger.error("Removing {} in userSessionsByConversationId for user {}", key, getUserLogin(session));
+                userSessionsByConversationId.remove(key);
+            });
+        }
+    }
+
+    private <T> Set<T> getKeysToRemove(Set<Map.Entry<T, UserSession>> entries){
+        Set<T> sessionKeysToRemove = new HashSet<>();
+        entries.forEach(entry -> {
+            UserSession session = entry.getValue();
+            if (doesSessionShouldBeRemoved(session)){
+                sessionKeysToRemove.add(entry.getKey());
+            }
+        });
+        return sessionKeysToRemove;
+    }
+
+
+    private boolean doesSessionShouldBeRemoved(UserSession userSession) {
+        return userSession.hasNotBeenUsedIn(emergencyRemovalTimeUnusedSessionInSeconds *1000);
+    }
+
+    private String getUserLogin(UserSession userSession){
+        try{
+            return userSession.getSystemUser().getLogin();
+        }catch (Exception ex){
+            return "unkwnon user";
+        }
     }
 
 }
