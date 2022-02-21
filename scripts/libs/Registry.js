b1026d1a0309254659e7db1bdece45150832ef18
Jakub Kosecki <jakub.kosecki0520@gmail.com>
2022-02-21 09:32:35 +0100

    added fh-cli

---
+++ scripts/libs/Registry.js
@@ -0,0 +1,241 @@
+const fs = require('fs');
+const utils = require('./Utils');
+const network = require('./Network');
+const path = require('path')
+
+const REGISTRY_PATH = process.env.REGISTRY_PATH || 'https://registry.npmjs.org';
+const LOCAL_REGISTRY_PATH = process.env.LOCAL_REGISTRY_PATH || `${require('os').homedir()}/.node_registry`;
+
+const _isFH = (name) => {
+  const fhModules = require('../data/packages_list.json');
+  return fhModules.pkgs.includes(name);
+}
+
+const _extractVersionFromFileName = (file) => {
+  file = file.split(path.sep)[file.split(path.sep).length - 1];
+  const libName = file.split(/-\d+\./)[0];
+  const version = file.replace(`${libName}-`, '').replace('.tgz', '');
+  return version;
+}
+
+const getLocalRegistry = () => {
+  if (!fs.existsSync(LOCAL_REGISTRY_PATH)) {
+    fs.mkdirSync(LOCAL_REGISTRY_PATH);
+  }
+  const subdirs = fs.readdirSync(LOCAL_REGISTRY_PATH);
+  const objs = subdirs.map(file => {
+    if (file.endsWith('.tgz')) {
+      const libName = file.split(/-\d+\./)[0];
+      const version = file.replace(`${libName}-`, '').replace('.tgz', '');
+      return {
+        name: libName,
+        version,
+        absolutePath: `${LOCAL_REGISTRY_PATH}/${file}`
+      }
+    }
+    return null;
+  }).filter(el => !!el);
+  const libs = [];
+  objs.forEach(lib => {
+    const exLib = libs.find(el => el.name === lib.name);
+    if (!exLib) {
+      libs.push({
+        name: lib.name,
+        versions: [
+          {
+            version: lib.version,
+            absolutePath: lib.absolutePath
+          }
+        ]
+      });
+    } else {
+      exLib.versions.push({
+        version: lib.version,
+        absolutePath: lib.absolutePath
+      })
+    }
+  });
+  return libs;
+}
+
+const useLocalRegistry = async (packagePath, emergencyAction = 'none') => {
+  if (!fs.existsSync(packagePath)) {
+    console.error(`File ${packagePath} not extists!`);
+    return;
+  }
+
+  if (!['none', 'next', 'latest'].includes(emergencyAction)) {
+    console.error(`Priority should be one of: [none, next, latest]`);
+    return
+  }
+
+  const pkg = utils.readJSON(packagePath);
+  const localRegistry = getLocalRegistry();
+  if (pkg.dependencies) {
+    for (const pkgDep of Object.entries(pkg.dependencies)) {
+      let [name, version] = pkgDep;
+      if (!version.startsWith('file:') && _isFH(name)) {
+        let desiredVersion = `file:${LOCAL_REGISTRY_PATH}${path.sep}${name}-${version}.tgz`;
+        let canChange = true;
+        version = version.replace('^', '');
+        let pkgExists = true;
+        let lPkgObjTemp = localRegistry.find(lpkg => lpkg.name === name);
+        let lPkgObj;
+        if (!lPkgObjTemp) {
+          pkgExists = false;
+        } else {
+          lPkgObj = lPkgObjTemp.versions.find(lpkg => lpkg.version === version);
+          if (!lPkgObj) {
+            pkgExists = false;
+          }
+        }
+        if (!pkgExists) {
+          const remoteData = await network.get({
+            address: `${REGISTRY_PATH}/${name}`,
+          });
+          let remoteVersion = remoteData.versions[version];
+          if (!remoteVersion) {
+            console.error(`Version ${version} of package ${name} not exists localy or remotely!`);
+            if (emergencyAction === 'none') {
+              console.log('leaving as it is!');
+              canChange = false;
+            } else if (['next', 'latest'].includes(emergencyAction)) {
+              const v = remoteData['dist-tags'][emergencyAction];
+              if (!v) {
+                console.log('leaving as it is!');
+                canChange = false;
+              } else {
+                remoteVersion = remoteData.versions[v];
+                if (!remoteVersion) {
+                  console.log('leaving as it is!');
+                  canChange = false;
+                } else {
+                  desiredVersion = `file:${LOCAL_REGISTRY_PATH}${path.sep}${name}-${v}.tgz`
+                  const pkgUrl = remoteVersion.dist.tarball;
+                  console.log(`Downloading (${emergencyAction}) ${name}@${v}`);
+                  await network.download(pkgUrl, LOCAL_REGISTRY_PATH, `${name}-${v}.tgz`);
+                }
+              }
+            }
+          } else {
+            const pkgUrl = remoteVersion.dist.tarball;
+            console.log(`Downloading ${name}@${version}`);
+            await network.download(pkgUrl, LOCAL_REGISTRY_PATH, `${name}-${version}.tgz`);
+          }
+        }
+        if (canChange) {
+          pkg.dependencies[name] = desiredVersion;
+        }
+      }
+    }
+  }
+  utils.writeJSON(packagePath, pkg);
+}
+
+const useRemoteRegistry = async (packagePath, fallback = 'none') => {
+  if (!fs.existsSync(packagePath)) {
+    console.error(`File ${packagePath} not extists!`);
+    return;
+  }
+
+  const pkg = utils.readJSON(packagePath);
+  if (pkg.dependencies) {
+    for (const pkgDep of Object.entries(pkg.dependencies)) {
+      let [name, version] = pkgDep;
+      if (version.startsWith('file:') && _isFH(name)) {
+        const v = _extractVersionFromFileName(version);
+        const remoteData = await network.get({
+          address: `${REGISTRY_PATH}/${name}`,
+        });
+        if (!!remoteData.versions[v]) {
+          pkg.dependencies[name] = v;
+        } else if (fallback !== 'none') {
+          const newV = remoteData['dist-tags'][fallback];
+          if (!!remoteData.versions[newV]) {
+            pkg.dependencies[name] = newV;
+          }
+        }
+      }
+    }
+  }
+  
+  utils.writeJSON(packagePath, pkg);
+}
+
+const localDeploy = (packagePath, timestamp, address, verbose) => {
+  if (!fs.existsSync(packagePath)) {
+    console.error(`File ${packagePath} not extists!`);
+    return;
+  }
+  const pkg = utils.readJSON(packagePath);
+  let actV;
+  if (pkg.version.includes('-SNAPSHOT')) {
+    actV = pkg.version.split('-SNAPSHOT')[0];
+  } else {
+    actV = pkg.version;
+  }
+  pkg.version = `${actV}-SNAPSHOT-${timestamp}`;
+  utils.writeJSON(packagePath, pkg);
+
+  const dirPath = packagePath.replace('package.json', '');
+  process.chdir(dirPath);
+  utils.runProcess(`npm install --registry ${address || REGISTRY_PATH}`, verbose);
+  utils.runProcess(`npm pack`, verbose);
+  const pkgName = `${pkg.name}-${pkg.version}.tgz`
+  const tgzSourcePath = `${dirPath}${path.sep}${pkgName}`;
+  const tgzDestPath = `${LOCAL_REGISTRY_PATH}${path.sep}${pkgName}`;
+  utils.runProcess(`cp ${tgzSourcePath} ${tgzDestPath}`, verbose);
+  utils.runProcess(`rm ${tgzSourcePath}`, verbose);
+
+  return {name: pkg.name, version: tgzDestPath};
+}
+
+const updateDependencies = ({name, version}, pkgs) => {
+  for (const pkg of pkgs) {
+    if (!fs.existsSync(pkg)) {
+      console.error(`File ${pkg} not extists!`);
+      return;
+    }
+  
+    const pkgJSON = utils.readJSON(pkg);
+    if (pkgJSON.dependencies && pkgJSON.dependencies[name]) {
+      pkgJSON.dependencies[name] = `file:${version}`;
+    }
+    utils.writeJSON(pkg, pkgJSON);
+  }
+}
+
+const updateToNewestVersion = async (packagePath, fallback = 'next') => {
+  if (!fs.existsSync(packagePath)) {
+    console.error(`File ${packagePath} not extists!`);
+    return;
+  }
+
+  const pkg = utils.readJSON(packagePath);
+  if (pkg.dependencies) {
+    for (const name of Object.keys(pkg.dependencies)) {
+      if (_isFH(name)) {
+        const remoteData = await network.get({
+          address: `${REGISTRY_PATH}/${name}`,
+        });
+        const newV = remoteData['dist-tags'][fallback];
+        if (!!remoteData.versions[newV]) {
+          pkg.dependencies[name] = newV;
+        } else {
+          console.log(`There is no version @${fallback} for lib ${name}`)
+        }
+      }
+    }
+  }
+  utils.writeJSON(packagePath, pkg);
+}
+
+module.exports = {
+  LOCAL_REGISTRY_PATH,
+  getLocalRegistry,
+  useLocalRegistry,
+  useRemoteRegistry,
+  localDeploy,
+  updateDependencies,
+  updateToNewestVersion
+};
---
+++ scripts/libs/Runtime.js
@@ -0,0 +1,253 @@
+const utils = require('./Utils');
+const registry = require('./Registry');
+const packages_list = require('../data/packages_list.json');
+const pth = require('path');
+const fs = require('fs');
+
+const helpTable = {
+  switchToLocal: `
+This command swithces all fh paths to local ones.
+Supported params: 
+--priority none|next|latest - setups action priority of non existing component,
+                              Means: when switch will not find local version of 
+                              dependency lib and it will be not available on remote 
+                              registry, then it will try:
+                                
+                                none - do nothing, just skip,
+                                next - download version tagged as next,
+                                latest - download version tagged as latest,
+--verbose - enables verbose log
+  `,
+
+  switchToRemote: `
+This command swithces all fh paths to remote ones.
+Supported params: 
+--verbose - enables verbose log
+--priority none|next|latest - setups action priority of non existing remotely component,
+                              Means: when switch will not find remote version of 
+                              dependency lib registry, then it will:
+                                
+                                none - do nothing, just skip,
+                                next - download version tagged as next,
+                                latest - download version tagged as latest,
+  `,
+
+updateToNewestVersion: `
+  This command update all fh paths to remote ones with provided tag.
+  Supported params: 
+  --verbose - enables verbose log
+  --priority next|latest - setups action priority of non existing remotely component,
+                                Means: when switch will not find remote version of 
+                                dependency lib registry, then it will:
+                                  
+                                next - download version tagged as next,
+                                latest - download version tagged as latest,
+    `,
+
+  deploy: `
+This command deploys all packages to npm repository.
+Supported params: 
+--address={verdaccio address} - default is set to npmjs registry,
+--fhVer={new fh version} - if not setted will be getted from registry,
+--prod - setup as prod
+--snap - setup as snapshot
+--verbose - enables verbose log
+--dryRun - enables test build without publish
+  `,
+
+  deployLocal: `
+This command deploys all packages to local registry.
+Supported params: 
+--address={verdaccio address} - default is set to npmjs registry,
+--verbose - enables verbose log
+  `
+
+}
+
+class Runtime {
+  constructor() {
+    const nodeV = process.version.match(/^v(\d+)/)[1];
+    if (nodeV !== '12') {
+      throw new Error('Please use node js in version 12!');
+    }
+  }
+
+  _run(processName, params) {
+    const fn = this[processName];
+    if (fn) {
+      if (params.has('help')) {
+        this._help(params.processName);
+      } else {
+        fn(params);
+      }
+    }
+  }
+
+  _help(pName) {
+    console.log(helpTable[pName]);
+  }
+
+  deploy(params) {
+    const MAIN_PATH = process.cwd();
+    const FH_DIRS = packages_list.dirs;
+    const FH_PACKAGES = packages_list.pkgs;
+    const snapId = +new Date();
+    let address = 'https://registry.npmjs.org/';
+    if (params.has('address')) {
+      if (utils.validURL(params.get('address'))) {
+        address = params.get('address');
+      } else {
+        console.error(`Address ${params.get('address')} is invalid!`);
+        return;
+      }
+    }
+
+    let fhVer = params.get('fhVer');
+    const isProd = params.has('prod');
+    let isSnapshot = params.has('snap');
+    const verbose =  params.has('verbose');
+    if (!isProd && !isSnapshot) {
+      isSnapshot = true;
+    }
+
+    if (isProd) {
+      if (!fhVer) {
+        const cPack = JSON.parse(fs.readFileSync(`${process.cwd()}/${FH_DIRS[0]}package.json`));
+        fhVer = cPack.version.split('-')[0];
+      }
+    }
+
+    if (isSnapshot) {
+      if (!fhVer) {
+        const cPack = JSON.parse(fs.readFileSync(`${process.cwd()}/${FH_DIRS[0]}package.json`));
+        console.log(cPack);
+        fhVer = cPack.version.split('-')[0];
+      }
+      fhVer = `${fhVer}-SNAPSHOT-${snapId}`;
+    }
+
+    let shouldPublish = true;
+    if (params.has('dryRun')) {
+      shouldPublish = false;
+    }
+
+    const publishProcess = (path, onlyUpdate) => {
+      process.chdir(pth.join(MAIN_PATH, path));
+      const name = path.split(pth.sep).splice(-2, 1);
+      console.log('\n\n-----------------------------------------------')
+      console.log(`${`-- deploing: ${name} ${fhVer}`.padEnd(45, ' ')}--`)
+      console.log('-----------------------------------------------\n\n')
+      let pack;
+      if (fhVer) {
+        pack = JSON.parse(fs.readFileSync('package.json'));
+        pack.version = fhVer;
+        if (pack.publishConfig && pack.publishConfig.registry) {
+          pack.publishConfig.registry = address;
+        }
+        for (const dep of Object.keys(pack.dependencies)) {
+          if (FH_PACKAGES.includes(dep)) {
+            pack.dependencies[dep] = fhVer;
+          }
+        }
+        fs.writeFileSync('package.json', JSON.stringify(pack, null, 2));
+      }
+      utils.runProcess('rm -fr node_modules package-lock.json', verbose);
+      if (shouldPublish && !onlyUpdate) {
+        console.log(process.cwd())
+        utils.runProcess(`npm install --registry ${address}`, verbose);
+        
+        let tag;
+        if (isSnapshot) {
+          tag = 'next';
+        } else if (isProd) {
+          tag = 'latest';
+        }
+    
+        utils.runProcess(`npm run build`, verbose);
+        utils.runProcess(`npm publish --force --tag ${tag} --registry ${address}`, verbose);
+        utils.runProcess('rm -fr node_modules package-lock.json', verbose);
+      }
+    }
+
+    const searchForFile = (mainDir, packageFiles) => {
+      fs.readdirSync(mainDir).forEach(file => {
+        if (file === 'node_modules' || file === 'target') {
+          return;
+        }
+        if (file === 'package.json') {
+          // console.log(pth.join(mainDir, file))
+          packageFiles.push(pth.join(mainDir, file));
+        } else {
+          const fPath = pth.join(mainDir, file);
+          if (fs.statSync(fPath).isDirectory()) {
+            searchForFile(fPath, packageFiles);
+          }
+        }
+      })
+      return packageFiles;
+    }
+
+    const SKIP_PUBLISH_FH_DIRS = [
+      '../packages/applications/default-application/'
+    ]
+
+    for (const path of FH_DIRS) {
+      publishProcess(path, SKIP_PUBLISH_FH_DIRS.includes(path));
+    }
+
+    console.log(`NEW FH VERSION: ${fhVer}`);
+  }
+
+  async switchToLocal(params) {
+    const pkgs = await utils.getPackages('../', params.has('verbose'));
+    for (const pkg of pkgs) {
+      console.log(`Switching ${pkg} to local registry...`);
+      await registry.useLocalRegistry(pkg, params.get('priority'));
+      console.log(`Switching ${pkg} to local registry - DONE!`);
+    }
+  }
+
+  async switchToRemote(params) {
+    const pkgs = await utils.getPackages('../', params.has('verbose'));
+    console.log('\n');
+    for (const pkg of pkgs) {
+      console.log(`Switching ${pkg} to remote registry...`);
+      await registry.useRemoteRegistry(pkg, params.get('priority'));
+      console.log(`Switching ${pkg} to remote registry - DONE!`);
+    }
+  }
+
+  async deployLocal(params) {
+    const pkgs = await utils.getPackages('../', params.has('verbose'));
+    const tstmp = +new Date();
+    const paths = packages_list.dirs.map(path => pth.join(process.cwd(), path));
+    console.log(paths);
+    for (const path of paths) {
+      console.log(`deployng local package on path (${path})`)
+      const result = registry.localDeploy(`${path}${pth.sep}package.json`, tstmp, params.get('address'), params.has('verbose'));
+      registry.updateDependencies(result, pkgs);
+    }
+  }
+
+  async updateToNewestVersion(params) {
+    const pkgs = await utils.getPackages('../', params.has('verbose'));
+    console.log('\n');
+    if (!['next', 'latest'].includes(params.get('priority')) || params.get('priority') === undefined) {
+      console.log(`--priority is wrong! Possible values are [next, latest]`);
+      return;
+    }
+    for (const pkg of pkgs) {
+      console.log(`Switching ${pkg} to remote registry...`);
+      await registry.updateToNewestVersion(pkg, params.get('priority'));
+      console.log(`Switching ${pkg} to remote registry - DONE!`);
+    }
+  }
+
+  clearLocalRegistry(params) {
+    console.log(`Deleting registry file on path ${registry.LOCAL_REGISTRY_PATH}`);
+    utils.runProcess(`rm -fr ${registry.LOCAL_REGISTRY_PATH}/*`, params.has('verbose'));
+    console.log('Done!')
+  }
+}
+
+module.exports = new Runtime();
