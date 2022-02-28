5601b5c9811b6793a8c30732eac7789fc4d2c557
kurzac <kurzac@wp.pl>
2022-02-28 17:20:41 +0100

    Tools for test

--- example/src/main/java/pl/fhframework/example/FhDemoUC.java
+++ example/src/main/java/pl/fhframework/example/FhDemoUC.java
@@ -1,19 +1,27 @@
 package pl.fhframework.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.fhframework.UserSession;
 import pl.fhframework.annotations.Action;
+import pl.fhframework.core.session.UserSessionRepository;
 import pl.fhframework.core.uc.IInitialUseCase;
 import pl.fhframework.core.uc.UseCase;
 
 
 @UseCase
 public class FhDemoUC implements IInitialUseCase {
+    @Autowired
+    private UserSessionRepository userSessionRepository;
+
     @Override
     public void start() {
+        UserSession userSession = userSessionRepository.getAllUserSessions().stream().findFirst().get();
+        userSession.getAttributes().put("id", "123");
         showForm(FhDemoForm.class, "");
     }
 
     @Action
     public void onClose() {
-        exit();
+        userSessionRepository.removeObsoleteSessionsWithAttribute("id", "123");
     }
 }
