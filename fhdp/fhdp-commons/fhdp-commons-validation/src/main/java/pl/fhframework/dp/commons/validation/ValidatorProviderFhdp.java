18b660f5f61631182f4795da6b5e2bf6084505c4
Jacek Borowiec <jacek.borowiec@gmail.com>
2022-03-31 10:39:00 +0200

    [feature](NO_TASK): Multi-language JSR validation

--- fhdp/fhdp-commons/fhdp-commons-validation/src/main/java/pl/fhframework/dp/commons/validation/ValidatorProviderFhdp.java
+++ fhdp/fhdp-commons/fhdp-commons-validation/src/main/java/pl/fhframework/dp/commons/validation/ValidatorProviderFhdp.java
@@ -8,12 +8,15 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MessageSourceResourceBundleLocator;
+import pl.fhframework.dp.commons.utils.lng.LngDescription;
+import pl.fhframework.dp.commons.utils.lng.LngDescriptionHolder;
 
-import javax.validation.MessageInterpolator;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import javax.validation.*;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @Slf4j
@@ -36,5 +39,37 @@ public class ValidatorProviderFhdp {
                 .usingContext()
                 .messageInterpolator(new MessageInterpolatorFhdp(locale, new MessageSourceResourceBundleLocator(validationTranslationsMessageSource)))
                 .getValidator();
+    }
+
+    /**
+     * Provides multi-language JSR validation result set for object.
+     * The reason is, that evaluation of JSR validation message keys is impossible without validation context.
+     * Key in result map contains pointer, value, LngDescriptionHolder with descriptions. It can be translated to Json
+     * by LngDescriptionUtil.toJson() method.
+     *
+     * @param object validated object
+     * @param languages list of languages for descriptions
+     * @return
+     */
+    public Map<String, LngDescriptionHolder> validateJsr(Object object, List<String> languages) {
+        Map<String, LngDescriptionHolder> ret = new ConcurrentHashMap<>();
+        for(String lng: languages) {
+            Set<ConstraintViolation<Object>> resultSet = getValidator(new Locale(lng))
+                    .validate(object);
+            registerDescriptions(resultSet, lng, ret);
+        }
+        return ret;
+    }
+
+    private void registerDescriptions(Set<ConstraintViolation<Object>> resultSet, String lng, Map<String, LngDescriptionHolder> errors) {
+        for(ConstraintViolation<Object> v: resultSet ) {
+            LngDescriptionHolder holder = errors.get(v.getPropertyPath().toString());
+            if(holder == null) holder = new LngDescriptionHolder();
+            LngDescription description = new LngDescription();
+            description.setLng(lng);
+            description.setText(v.getMessage());
+            holder.getDescriptions().add(description);
+            errors.put(v.getPropertyPath().toString(), holder);
+        }
     }
 }
