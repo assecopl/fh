32c8f1d56d2dacaf07e963042ef9a0ef404c8dda
k-czajka <k.czajka@doittechnology.pl>
2022-04-11 21:39:33 +0200

    Fixed substantive parameter combo null value

--- fhdp/fhdp-commons/fhdp-commons-fh/src/main/java/pl/fhframework/dp/commons/fh/parameters/details/SubstantiveParametersDetailForm.java
+++ fhdp/fhdp-commons/fhdp-commons-fh/src/main/java/pl/fhframework/dp/commons/fh/parameters/details/SubstantiveParametersDetailForm.java
@@ -3,6 +3,14 @@ package pl.fhframework.dp.commons.fh.parameters.details;
 import pl.fhframework.dp.commons.fh.parameters.list.SubstantiveParametersListModel;
 import pl.fhframework.model.forms.Form;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class SubstantiveParametersDetailForm extends Form<SubstantiveParametersListModel> {
 
+    public List<String> getTags() {
+        return Optional.ofNullable(this.getModel().getSelectedSubstantiveParametersDto().getTags())
+                .orElse(new ArrayList<>());
+    }
 }
