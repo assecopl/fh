51e836ac7fba8d664740e608272e3f4c0a90f66f
Paweł Domański <pawel.domanski@asseco.pl>
2022-06-03 11:05:05 +0200

    [fix](NO_TASK): Fix deselection of elements in TablePaged when click on one specific row.

--- packages/components/basic-controls/source/controls/Table/Table.ts
+++ packages/components/basic-controls/source/controls/Table/Table.ts
@@ -324,9 +324,9 @@ class Table extends TableWithKeyboardEvents {
                 }
 
                 if (this._formId === 'FormPreview') {
-                    this.fireEvent('onRowClick', this.onRowClick);
+                    this.fireEvent('onChange', this.onRowClick);
                 } else {
-                    this.fireEventWithLock('onRowClick', this.onRowClick, event);
+                    this.fireEventWithLock('onChange', this.onRowClick, event);
                 }
             }.bind(this));
             row.contentWrapper.insertBefore(cell, row.contentWrapper.firstChild);
