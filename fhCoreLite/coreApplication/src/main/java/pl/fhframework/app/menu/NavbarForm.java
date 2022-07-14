a6631fb20f0a70b3b35c9726b2813d5f88a4ce9c
Paweł Domański <pawel.domanski@asseco.pl>
2022-07-14 14:54:24 +0200

    [feature](#124): extend localization engine

--- fhCoreLite/coreApplication/src/main/java/pl/fhframework/app/menu/NavbarForm.java
+++ fhCoreLite/coreApplication/src/main/java/pl/fhframework/app/menu/NavbarForm.java
@@ -36,7 +36,10 @@ public class NavbarForm extends Form<NavbarForm.Model> {
     }
 
     public enum Language {
-        POLISH("pl"), ENGLISH("en");
+        POLISH("pl"), ENGLISH("en"),
+        NO("no"),
+
+        VIEWKEY("viewkey");
 
         private String value;
 
