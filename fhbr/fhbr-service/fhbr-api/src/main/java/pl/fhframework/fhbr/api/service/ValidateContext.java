4c0296e03dae2868776c491d5af266f9ba9886b8
Dariusz Skrudlik <dariusz.skrudlik@gmail.com>
2022-08-18 15:40:01 +0200

    ValidateContext

--- fhbr/fhbr-service/fhbr-api/src/main/java/pl/fhframework/fhbr/api/service/ValidateContext.java
+++ fhbr/fhbr-service/fhbr-api/src/main/java/pl/fhframework/fhbr/api/service/ValidateContext.java
@@ -15,6 +15,8 @@
 
 package pl.fhframework.fhbr.api.service;
 
+import java.util.List;
+
 /**
  * Context for single validation request.
  *
@@ -24,5 +26,34 @@ package pl.fhframework.fhbr.api.service;
  */
 public interface ValidateContext {
 
+    /**
+     * Getter for MessageFactory.
+     * <p>
+     * Use for create ValidationMessage inside rule body.
+     * e.g.
+     * ValidationMessage m = context.getMessageFactory().newInstance();
+     * ... // set severity / message / pointer etc.
+     * validationResult.addValidationMessage(m);
+     *
+     * @return
+     */
     ValidationMessageFactory getMessageFactory();
+
+    /**
+     * Apply additional rule set during current validation process
+     * - on this same or new (other) object
+     * <p>
+     * Usage during rule typu 'flow'' (ComplexRule), where additional decision must be done
+     * before run other rule set.
+     * <p>
+     * Result list of ValidationMessage aren't rewrite to validationResult.
+     * It can be done manually:
+     * e.g. validationMessages.stream().forEach(vm -> validationResult.addValidationMessage(vm));
+     *
+     * @param ruleSetCode    - any ruleSet
+     * @param phase          - any phase
+     * @param validateObject - target object wrapper
+     * @return list of ValidationMessages
+     */
+    List<ValidationMessage> applyRules(String ruleSetCode, String phase, ValidateObject validateObject);
 }
