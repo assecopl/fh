f243f54b1e3a2a4b2943efd7520c1cfce8c6b093
Pawel Domanski <pawel.domanski@asseco.pl>
2023-11-17 14:21:43 +0100

    [feature](Angular) -  DictionaryLookup component logic

--- packages_angular/fh-angular/libs/fh-ng-core-lite/src/lib/controls/form/form.component.ts
+++ packages_angular/fh-angular/libs/fh-ng-core-lite/src/lib/controls/form/form.component.ts
@@ -87,7 +87,6 @@ export class FormComponent extends FhngComponent implements OnInit, OnChanges, O
                         }
                     }
                 })
-
             }
         })
 
