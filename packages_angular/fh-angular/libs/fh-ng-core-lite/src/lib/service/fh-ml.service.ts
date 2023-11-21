e4ac9bfb66b3310f902b5099db510f36288131ce
Pawel Domanski <pawel.domanski@asseco.pl>
2023-11-21 11:23:27 +0100

    [feature](Angular) - FHML extend

--- packages_angular/fh-angular/libs/fh-ng-core-lite/src/lib/service/fh-ml.service.ts
+++ packages_angular/fh-angular/libs/fh-ng-core-lite/src/lib/service/fh-ml.service.ts
@@ -308,21 +308,6 @@ export class FhMLService {
         },
         noContent: false,
       },
-
-      {
-        //Blocquote footer / footer
-        tag: 'bqFooter',
-        tagConstructor: (match, contents) => {
-          var blockquote = document.createElement('footer');
-          blockquote.classList.add('fhml');
-          blockquote.classList.add('blockquote-footer');
-
-          blockquote.innerHTML = contents;
-
-          return blockquote.outerHTML;
-        },
-        noContent: false,
-      },
       {
         //Blocquote footer / footer
         tag: 'small',
@@ -394,6 +379,183 @@ export class FhMLService {
         },
 
         noContent: false,
+      },
+      {
+        tag: 'portal',
+        tagConstructor: (match, attr) => {
+          console.warn('THIS PORTAL TAG IS DEPRECATED. PLEASE USE fhportal!');
+          const attrList = attr.split(',');
+          const idPart = attrList[0];
+          const portalId = document.querySelectorAll(`[id^='${idPart}']`)[0].id;
+          const portalElement = document.getElementById(portalId);
+          const parentPortalElement = (portalElement.parentNode as HTMLElement);
+          parentPortalElement.style.display = "none";
+          const newElement = (portalElement.cloneNode(true) as HTMLElement);
+          newElement.id = `${portalElement.id}_portal`;
+
+          EVENTS_LIST.forEach(el => {
+            newElement.setAttribute(`on${el}`, `document.getElementById("${portalElement.id}").${el}();`);
+          })
+
+          if (attrList.length > 1) {
+            const stylesList: string[] = attrList[1].split(' ');
+            stylesList.forEach(styleName => {
+              newElement.classList.add(styleName);
+            });
+          }
+
+          return newElement.outerHTML;
+        },
+        attr: '([a-zA-Z0-9\-\, ]+)',
+        noContent: true
+      },
+      {
+        tag: 'fhportal',
+        tagConstructor: (match, attr) => {
+          new Portal();
+          try {
+            const parseAttr = (attr: string): {
+              v?: string,
+              layer?: string,
+              wrapped?: string,
+              id?: string,
+              classes?: string,
+              replaceParentId?: string,
+              removeWrapper?: string,
+              searchParent?: string,
+              searchClosestLike?: string
+            } => {
+              console.log(attr);
+              const res = {};
+              const variables = attr.split(';');
+              for (const variable of variables) {
+                let [key, value] = variable.split('=')
+                if (value[0] === '[' && value[value.length - 1] === ']') {
+                  value = value.substr(1);
+                  value = value.substr(0, value.length - 1);
+                }
+                if (['id', 'classes', 'wrapped', 'layer', 'v', 'searchClosestLike', 'searchParent', 'replaceParentId', 'removeWrapper'].indexOf(key) > -1) {
+                  res[key] = value
+                }
+              }
+              return res;
+            }
+            const meta = parseAttr(attr)
+            const portal = document.createElement('fh-portal')
+            portal.setAttribute('used', 'false');
+            if (meta.id) {
+              portal.setAttribute('objId', meta.id);
+            } else {
+              return '<div>NO fhportal ID!</div>'
+            }
+            if (meta.searchClosestLike) {
+              portal.setAttribute('searchClosestLike', meta.searchClosestLike);
+            }
+            if (meta.searchParent) {
+              portal.setAttribute('searchParent', meta.searchParent);
+            }
+            if (meta.classes) {
+              portal.setAttribute('classes', meta.classes);
+            }
+            if (meta.wrapped) {
+              portal.setAttribute('wrapped', meta.wrapped);
+            }
+            if (meta.layer) {
+              portal.setAttribute('layer', meta.layer);
+            }
+            if (meta.v) {
+              portal.setAttribute('v', meta.v);
+            }
+            if (meta.replaceParentId) {
+              portal.setAttribute('replaceParentId', meta.replaceParentId)
+            }
+            if (meta.removeWrapper) {
+              portal.setAttribute('removeWrapper', meta.removeWrapper)
+            }
+            return portal.outerHTML;
+          } catch (e: any) {
+            console.error(e.message);
+            return '<div>Error portal</div>'
+          }
+        },
+        attr: '(.+)',
+        noContent: true
+      },
+      {
+        tag: 'nbsp',
+        tagConstructor: (match, attr) => {
+          const repTime = Number(attr);
+          if (!isNaN(repTime)) {
+            var span = document.createElement('span');
+            span.style.paddingLeft = `${repTime}em`;
+            return span.outerHTML;
+          }
+          return '';
+        },
+        attr: '([a-zA-Z0-9\-\, ]+)',
+        noContent: true
+      },
+      {
+        tag: 'unescape',
+        tagConstructor: (match, attr) => {
+          const code = String(attr);
+          const span = document.createElement('span');
+          span.innerHTML += `&#${code};`;
+          return span.outerHTML;
+        },
+        attr: '([a-zA-Z0-9\-\, ]+)',
+        noContent: true
+      },
+      {
+        tag: 'ahref',
+        tagConstructor: (match, attr, contents) => {
+          try {
+            const parseAttr = (attr: string): { href?: string, alt?: string, target?: string } => {
+              const res = {};
+              const variables = attr.split(';');
+              for (const variable of variables) {
+                let [key, value] = variable.split('=')
+                if (value[0] === '[' && value[value.length - 1] === ']') {
+                  value = value.substr(1);
+                  value = value.substr(0, value.length - 1);
+                }
+                if (['href', 'alt', 'target'].indexOf(key) > -1) {
+                  res[key] = value
+                }
+              }
+              return res;
+            }
+
+            const meta = parseAttr(attr)
+            const aElement = document.createElement('a');
+
+            if (meta.href) {
+              aElement.href = meta.href;
+            }
+            if (meta.alt) {
+              aElement.setAttribute('alt', meta.alt);
+            }
+            if (meta.target) {
+              aElement.setAttribute('target', meta.target);
+            }
+            if (!!contents) {
+              aElement.innerHTML = contents;
+            }
+            return aElement.outerHTML;
+          } catch (e: any) {
+            console.error(e.message);
+            return ''
+          }
+        },
+        attr: '(.+)',
+        noContent: false
+      },
+      {
+        tag: 'extAttributes/',
+        tagConstructor: (match, contents) => {
+          return '';
+        },
+        noContent: true
       },
     ];
 
@@ -495,6 +657,8 @@ export class FhMLService {
   }
 }
 
+export const EVENTS_LIST = ["Event", "abort", "afterprint", "animationend", "animationiteration", "animationstart", "beforeprint", "beforeunload", "blur", "canplay", "canplaythrough", "change", "click", "contextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchange", "ended", "error", "focus", "fullscreenchange", "fullscreenerror", "hashchange", "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata", "loadstart", "message", "offline", "online", "open", "pagehide", "pageshow", "paste", "pause", "play", "playing", "popstate", "progress", "ratechange", "resize", "reset", "scroll", "search", "seeked", "seeking", "select", "show", "stalled", "storage", "submit", "suspend", "timeupdate", "toggle", "touchcancel", "touchend", "touchmove", "touchstart", "unload", "volumechange", "waiting", "wheel", "altKey", "animationName", "bubbles", "button", "buttons", "cancelable", "charCode", "changeTouches", "clientX", "clientY", "clipboardData", "code", "composed", "ctrlKey", "currentTarget", "data", "dataTransfer", "defaultPrevented", "deltaX", "deltaY", "deltaZ", "deltaMode", "detail", "elapsedTime", "eventPhase", "inputType", "isComposing", "isTrusted", "key", "keyCode", "location", "lengthComputable", "loaded", "metaKey", "MovementX", "MovementY", "newValue", "newURL", "offsetX", "offsetY", "oldValue", "oldURL", "onemptied", "pageX", "pageY", "persisted", "propertyName", "pseudoElement", "region", "relatedTarget", "repeat", "screenX", "screenY", "shiftKey", "state", "storageArea", "target", "targetTouches", "timeStamp", "total", "touches", "type", "url", "which", "view"];
+
 type TagMetaType = {
   tag: string;
 
@@ -504,3 +668,182 @@ type TagMetaType = {
 
   noContent?: boolean;
 };
+
+//TODO Portal functionality need to bę rewriet fo Angular
+//@DEPRECATED
+export class Portal {
+  constructor() {
+    this.init();
+    window['fhportal'] = this;
+  }
+
+  private MAX_VERSION = 2;
+  private MIN_VERSION = 1;
+  public static OBSERVER_CONFIG = {attributes: true, childList: true, subtree: true};
+  public static OBSERVERS = [];
+
+  private VERSION_STORE = {
+    '1': this.version_1,
+    '2': this.version_2
+  }
+
+  init() {
+    if (!window['fhportal']) {
+      const config = {attributes: true, childList: true, subtree: true};
+      const observer = new MutationObserver(this.handleMutation);
+      observer.observe(document.body, Portal.OBSERVER_CONFIG);
+    }
+  }
+
+  version_1({sourceElement, portal, sourceId}) {
+    const sourceCoords = sourceElement.getBoundingClientRect() as any;
+    const portalCoords = portal.getBoundingClientRect() as any;
+    if (portalCoords.x !== sourceCoords.x || portalCoords.y !== sourceCoords.y) {
+      const newX = portalCoords.x - sourceCoords.x;
+      const newY = portalCoords.y - sourceCoords.y;
+      sourceElement.style.transform = `translate(${newX}px, ${newY}px)`;
+      if (document.getElementById(sourceId).blur) {
+        document.getElementById(sourceId).blur();
+      }
+    } else {
+      setTimeout(() => {
+        const sourceCoords = sourceElement.getBoundingClientRect() as any;
+        const portalCoords = portal.getBoundingClientRect() as any;
+        const newX = portalCoords.x - sourceCoords.x;
+        const newY = portalCoords.y - sourceCoords.y;
+        sourceElement.style.transform = `translate(${newX}px, ${newY}px)`;
+        if (document.getElementById(sourceId).blur) {
+          document.getElementById(sourceId).blur();
+        }
+      }, 500);
+    }
+    portal.setAttribute('used', 'true');
+  }
+
+  version_2({sourceElement, portal, sourceId}) {
+    const replaceParentId = portal.getAttribute('replaceParentId')
+    if (!!replaceParentId) {
+      const oldParent = document.getElementsByClassName(replaceParentId)[0];
+      if (!!oldParent) {
+        oldParent.parentElement.appendChild(portal)
+        oldParent.remove();
+      }
+    }
+
+    portal.parentElement.appendChild(sourceElement);
+    if (document.getElementById(sourceId) && document.getElementById(sourceId).blur) {
+      document.getElementById(sourceId).blur();
+    }
+    portal.style.display = 'none';
+    portal.setAttribute('used', 'true');
+    const observer = new MutationObserver((mutationList, observer) => {
+      if (!portal.parentElement.contains(sourceElement) && document.body.contains(portal)) {
+        console.log("yoy nasty bastard!");
+        portal.setAttribute('used', 'false');
+      } else if (!document.body.contains(portal) || !document.body.contains(sourceElement)) {
+        observer.disconnect();
+        Portal.OBSERVERS[sourceId] = undefined;
+      }
+    });
+    observer.observe(portal.parentElement, Portal.OBSERVER_CONFIG);
+    if (Portal.OBSERVERS[sourceId]) {
+      Portal.OBSERVERS[sourceId].disconnect();
+    }
+    Portal.OBSERVERS[sourceId] = observer;
+  }
+
+  processVersion(version: number, args: { [key: string]: any }) {
+    const f = this.VERSION_STORE[`${version}`];
+    console.log(version, args, f)
+    if (f) {
+      f(args)
+    }
+  }
+
+
+  private handleMutation = (mutationList, observer) => {
+    const portals = document.querySelectorAll('fh-portal[used="false"]');
+    portals.forEach(portal => {
+      try {
+        const broken = portal.getAttribute('broken');
+        if (!broken) {
+          const searchClosestLike = portal.getAttribute('searchClosestLike');
+          const searchParent = portal.getAttribute('searchParent');
+          const sourceId = portal.getAttribute('objid');
+          const classes = portal.getAttribute('classes');
+          const wrapped = portal.getAttribute('wrapped');
+          const layer = portal.getAttribute('layer');
+          const v = portal.getAttribute('v');
+          const removeWrapper = portal.getAttribute('removeWrapper');
+          let isWrapped = false;
+          let isRemoveWrapper = false;
+          let isSearchClosest = false;
+          let zIndex;
+          let version;
+          if (v !== undefined && !isNaN(Number.parseInt(v)) && Number.parseInt(v) >= this.MIN_VERSION && Number.parseInt(v) <= this.MAX_VERSION) {
+            version = Number(v);
+          } else {
+            version = this.MAX_VERSION;
+          }
+          if (searchClosestLike) {
+            isSearchClosest = searchClosestLike === 'true';
+          }
+          if (wrapped) {
+            isWrapped = wrapped === 'true';
+          }
+          if (removeWrapper) {
+            isRemoveWrapper = removeWrapper === 'true';
+          }
+          if (layer !== undefined) {
+            zIndex = parseInt(layer);
+          }
+          let properClasses;
+          if (classes) {
+            properClasses = classes.split(',');
+          }
+
+          let sourceElement;
+          if (isSearchClosest) {
+            if (!searchParent) {
+              console.error('missing searchParent!')
+            } else {
+              const parent = portal.closest(searchParent);
+              sourceElement = parent.querySelector(`[id^='${sourceId}']`)
+            }
+          } else {
+            sourceElement = document.getElementById(sourceId);
+          }
+          if (isWrapped) {
+            while (!sourceElement.parentElement.classList.contains('wrapper')) {
+              sourceElement = sourceElement.parentElement;
+            }
+            sourceElement = sourceElement.parentElement;
+          } else if (isRemoveWrapper) {
+            let parentWrapper = document.getElementById(sourceElement.id);
+            while (!parentWrapper.parentElement.classList.contains('wrapper')) {
+              parentWrapper = parentWrapper.parentElement;
+            }
+            parentWrapper.parentElement.remove();
+          }
+          if (zIndex) {
+            sourceElement.style.zIndex = zIndex;
+          }
+          if (properClasses) {
+            properClasses.forEach(cls => {
+              sourceElement.classList.add(cls);
+            });
+          }
+          if (sourceElement) {
+            console.log(version, {sourceElement, portal, sourceId, properClasses});
+            this.processVersion(version, {sourceElement, portal, sourceId, properClasses})
+          } else {
+            console.error(`There is no element (id: ${sourceId}) that can be portalled`);
+          }
+        }
+      } catch (e) {
+        portal.setAttribute('broken', 'true');
+        console.error(e);
+      }
+    })
+  }
+}
