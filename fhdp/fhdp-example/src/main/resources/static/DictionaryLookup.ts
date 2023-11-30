6e49c26dc171662607c6fa9ba09f97ffd836a3bf
Pawel Domanski <pawel.domanski@asseco.pl>
2023-11-30 15:58:46 +0100

    [feature](Angular) - DictionaryLookup.ts

---
+++ fhdp/fhdp-example/src/main/resources/static/DictionaryLookup.ts
@@ -0,0 +1,23 @@
+import {HTMLFormComponent} from 'fh-forms-handler';
+
+
+class DictionaryLookup extends HTMLFormComponent {
+
+    constructor(componentObj: any, parent: HTMLFormComponent) {
+        super(componentObj, parent);
+    }
+
+    create() {
+        // super.create();
+    }
+
+    update(change) {
+    }
+
+    display() {
+    }
+
+
+}
+
+export {DictionaryLookup}
