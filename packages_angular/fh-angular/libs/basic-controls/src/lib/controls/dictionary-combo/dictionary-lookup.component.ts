68b20a6323fd6829ecba135aa967cb39b6e1fb56
Dawid Pych <dawid.pych@asseco.pl>
2023-12-09 16:57:44 +0100

    [feature](Angular) DictionaryLookup

--- packages_angular/fh-angular/libs/basic-controls/src/lib/controls/dictionary-combo/dictionary-lookup.component.ts
+++ packages_angular/fh-angular/libs/basic-controls/src/lib/controls/dictionary-combo/dictionary-lookup.component.ts
@@ -1,14 +1,27 @@
-import {Component, forwardRef, Injector, Input, OnInit, Optional, SimpleChanges, SkipSelf,} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  forwardRef,
+  Injector,
+  Input,
+  OnInit,
+  Optional,
+  SimpleChanges,
+  SkipSelf, ViewChild,
+} from '@angular/core';
 import {NgSelectConfig} from '@ng-select/ng-select';
 import {FhngInputWithListC} from '../../models/componentClasses/FhngInputWithListC';
 import {FhngComponent, IDataAttributes} from '@fh-ng/forms-handler';
 import {BootstrapWidthEnum} from "../../models/enums/BootstrapWidthEnum";
+import {NgbDropdown} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: '[dictionary-lookup]',
   templateUrl: './dictionary-lookup.component.html',
   styleUrls: ['./dictionary-lookup.component.scss'],
   providers: [
+
     /**
      * Dodajemy deklaracje klasy ogólnej aby wstrzykiwanie i odnajdowanie komponentów wewnątrz siebie było możliwe.
      * Dzięki temu budujemy hierarchię kontrolek Fhng.
@@ -16,7 +29,34 @@ import {BootstrapWidthEnum} from "../../models/enums/BootstrapWidthEnum";
     {provide: FhngComponent, useExisting: forwardRef(() => DictionaryLookupComponent)},
   ],
 })
-export class DictionaryLookupComponent extends FhngInputWithListC implements OnInit {
+export class DictionaryLookupComponent extends FhngInputWithListC implements OnInit, AfterViewInit {
+  public override values: Array<any> = [];
+
+  public cursor: number = 0;
+  public multiselect: boolean = false;
+  public multiselectRawValue: string[] = [];
+  public dicTitle: string;
+  public columns: any[] = [];
+  public rows: any[] = [];
+  public page: number = null;
+  public pagesCount: number = null;
+  public tableIsVisible : boolean = false;
+  public orgRawValue: any;
+  public guuid: string;
+  public popperId: string;
+  public onBlurValue: string;
+  public onPageValue: number;
+  public onPageChange: string;
+  public onSelectValue: number;
+
+  @ViewChild('myDrop', { static: true, read: NgbDropdown })
+  public myDrop: NgbDropdown = null;
+
+  @Input()
+  public override displayFunction: any = null;
+
+  @Input()
+  public override displayExpression: any = 'lastName'; // TODO Convertery i formatter ??
 
   protected autocompleter: any;
   protected selectedIndexGroup: any;
@@ -34,11 +74,9 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
   protected freeTyping: boolean;
   protected tagslist: Array<string> = [];
   protected tagsInputData: any;
-  public multiselectRawValue: string[] = [];
   protected widthRatio: number;
   protected multiselectOldValue: any;
   protected changeToFired: boolean;
-
   protected onInputTimer: any;
   protected openOnFocus: boolean = true;
   protected readonly onInputTimeout: number;
@@ -46,34 +84,20 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
   private instance: any;
   private divTooltipId: any;
   private divTooltip: any;
-
-  public dicTitle: string;
-
-  public columns: any[] = [];
-  public rows: any[] = [];
   private popupOpen: boolean = false;
   private searchRequested: boolean;
-  public page: number = null;
-  public pagesCount: number = null;
-  public tableIsVisible : boolean = false;
-  public orgRawValue: any;
   private onSearchValue:String;
   private valueHasBeenChanged: boolean;
   private onRefreshValue: boolean;
-
   private isSearch: boolean = true;
   private popupColor?: string;
   private dirty: boolean = false;
   private languageWrapped: any;
   private valueFromChangedBinding: any;
   private displayOnlyCode: boolean;
-
   private _writingDebaunceTimer: any;
   private pageChangeClicked: boolean = false;
   private clickInPopup: boolean = false;
-
-  public guuid: string;
-  public popperId: string;
 
 
   constructor(
@@ -92,114 +116,23 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
   } = null;
 
 
-  public cursor: number = 0;
-  //@Input()
-  //public label:string;
-  //
-  public override values: Array<any> = [];
-
-  public multiselect: boolean = false;
-
-  @Input()
-  public override displayFunction: any = null;
-
-  @Input()
-  public override displayExpression: any = 'lastName'; // TODO Convertery i formatter ??
-
-  //@Input('formatter')
-  //public resultFormatter = (x: string | {name: string}) => {
-  //  if(typeof x === 'string'){
-  //    return x
-  //  } else {
-  //    return x[this.displayExpresion? this.displayExpresion: "name"]
-  //  }
-  //};
-
-  // public getValuesForCursor(): ComboListElement[] {
-  //     let values: ComboListElement[] = [];
-  //     const keys = Object.keys(this.filteredValues);
-  //     keys.forEach((group, index) => {
-  //         let vals = this.filteredValues[group]
-  //         vals.forEach((value1, index1) => {
-  //             let element: ComboListElement = Object.assign(new ComboListElement, value1);
-  //
-  //             element.index = index1;
-  //             element.group = group;
-  //             values.push(element);
-  //             this.preselectValue(element);
-  //         })
-  //     })
-  //
-  //
-  //     return values
-  // }
-
-  /**
-   * Used to preselect values on init since we got only string data(only targetValu) form backend.
-   * @param value
-   * @param element
-   */
-    // public preselectValue(element: ComboListElement) {
-    //     element.disabled = false;
-    //     element.selected = false;
-    //     if (!this.multiselect) {
-    //         if (typeof this.rawValue === "string") {
-    //             if (element.targetValue == this.rawValue) {
-    //                 this.rawValue = element
-    //                 element.disabled = true;
-    //                 element.selected = true;
-    //             }
-    //         }
-    //         if (this.rawValue instanceof ComboListElement) {
-    //             if (this.ngSelectCompareFunction(this.rawValue, element)) {
-    //                 this.rawValue = element
-    //                 element.disabled = true;
-    //                 element.selected = true;
-    //             }
-    //         }
-    //     } else {
-    //         if (this.rawValue instanceof Array) {
-    //             if (this.multiselectRawValue.includes(element.targetValue) && !this.rawValue.includes(element)) {
-    //                 this.rawValue.push(element);
-    //                 element.disabled = true;
-    //                 element.selected = true;
-    //             }
-    //         }
-    //     }
-    // }
-
   @Input('filterFunction')
   public filterFunction: (term: string, value: any) => boolean = (
     term: string,
     value: any
   ) => this.inputFormatter(value)?.toLowerCase().includes(term?.toLowerCase());
 
-  //@Input('formatter')
   public inputFormatter = (x: ComboListElement) => {
-
     return x.displayAsTarget ? x.targetValue : x.displayedValue;
-    ;
-
   };
 
-  // protected removeItem(item: ComboListElement, event) {
-  //     event.stopPropagation();
-  //     event.preventDefault();
-  //     const idx = this.rawValue.indexOf(item);
-  //     if (idx > -1) {
-  //         this.rawValue.splice(idx, 1);
-  //     }
-  //     this.rawValue = [...this.rawValue];
-  //     this.removedIndex = this.multiselectRawValue.indexOf(item.targetValue);
-  //     this.multiselectRawValue = this.rawValue.map(a => a.targetValue);
-  //     this.valueChanged = true;
-  //     this.addedTag = true;
-  //     this.values = [...this.values] //Force object reference change to update ng-select view.
-  //     this.onChangeEvent();
-  // }
-
-  override ngOnInit() {
+  public override ngOnInit() {
     super.ngOnInit();
+  }
+
+  public override ngAfterViewInit() {
+    super.ngAfterViewInit();
+    console.log('ngAfterViewInit', this.myDrop);
   }
 
   override ngOnChanges(changes: SimpleChanges) {
@@ -216,83 +149,6 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     this.onChangeEvent();
   }
 
-  // public override updateModel(selectedValue: ComboListElement | ComboListElement[]) {
-  //
-  //     if (!this.disabled) {
-  //         this.valueChanged = true;
-  //         if (selectedValue) {
-  //             if (this.multiselect && selectedValue instanceof Array) {
-  //                 let difference = selectedValue.filter(x => !this.multiselectRawValue.includes(x.targetValue));
-  //                 if (difference.length > 0) {
-  //                     this.toggleElementOnList(difference[0])
-  //                     this.rawValue = selectedValue
-  //                     this.selectedIndex = difference[0].index;
-  //                     this.selectedIndexGroup = difference[0].group;
-  //                     this.multiselectRawValue = selectedValue.map(a => a.targetValue);
-  //                     this.onChangeEvent();
-  //                 }
-  //             } else if (selectedValue instanceof ComboListElement && this.rawValue != selectedValue) {
-  //                 // this.toggleElementOnList(selectedValue)
-  //                 selectedValue.disabled = true;
-  //                 selectedValue.selected = true;
-  //                 this.rawValue = selectedValue
-  //                 this.selectedIndex = selectedValue.index;
-  //                 this.selectedIndexGroup = selectedValue.group;
-  //                 this.forceSendSelectedIndex = true;
-  //                 this.cleared = false;
-  //                 this.onChangeEvent();
-  //             }
-  //             this.values = [...this.values] //Force object reference change to update ng-select view.
-  //         } else {
-  //             this.cleared = true;
-  //             this.selectedIndex = null;
-  //             this.selectedIndexGroup = null;
-  //         }
-  //     }
-  // };
-
-  // override extractChangedAttributes() {
-  //     let attrs = {
-  //         blur: undefined,
-  //         cleared: undefined,
-  //         text: undefined,
-  //         addedTag: undefined,
-  //         selectedIndexGroup: undefined,
-  //         selectedIndex: undefined,
-  //         cursor: undefined,
-  //         removedIndex: undefined
-  //     };
-  //     if (this.valueChanged || this.forceSendSelectedIndex) {
-  //         if (this.cleared) {
-  //             attrs.cleared = this.cleared
-  //         } else if (this.selectedIndex != null) {
-  //             attrs.selectedIndexGroup = this.selectedIndexGroup;
-  //             attrs.selectedIndex = this.selectedIndex;
-  //         } else if (this.removedIndex != null) {
-  //             attrs.removedIndex = this.removedIndex;
-  //         }
-  //         attrs.addedTag = this.addedTag;
-  //
-  //
-  //         this.addedTag = false;
-  //         // this.oldValue = this.rawValue;
-  //         this.multiselectOldValue = this.multiselectRawValue;
-  //         this.forceSendSelectedIndex = false;
-  //         this.selectedIndexGroup = null;
-  //         this.selectedIndex = null;
-  //         this.removedIndex = null;
-  //         this.cleared = false;
-  //     }
-  //
-  //     return attrs;
-  // };
-
-  // override onChangeEvent() {
-  //     if (this.onChange && !this.disabled) {
-  //         this.fireEventWithLock('onChange', this.onChange);
-  //     }
-  // }
-
   override mapAttributes(data: IDataAttributes | any) {
     super.mapAttributes(data);
     if (data.rawValue===""){
@@ -308,26 +164,16 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     // }
   }
 
-  // public ngSelectCompareFunction(a: ComboListElement, b: ComboListElement) {
-  //     return a.targetValue === b.targetValue;
-  // }
-  //
-  // public toggleElementOnList(element: ComboListElement) {
-  //     this.values.forEach(value => {
-  //         if (this.ngSelectCompareFunction(element, value)) {
-  //             value.disabled = !value.disabled;
-  //             value.selected = !value.selected;
-  //         }
-  //     })
-  // }
-
   public override updateModel(event) {
     // if (!this.disabled) {
     this.rawValue = event.target.value;
     // }
   };
 
-  override onInputEvent(event) {
+  override onInputEvent(event, myDrop?: NgbDropdown) {
+    if(myDrop) {
+      myDrop.open();
+    }
     this.updateModel(event);
     this.onSearchValue = this.rawValue;
     this.tableIsVisible = true;
@@ -340,7 +186,7 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     if (this.onSelectValue !== null && this.onSelectValue !== undefined) {
       attrs["command"] = "selectItem";
       attrs["select"] = this.onSelectValue;
-      this.resetValueChanged();
+      this._resetValueChanged();
       this.onSelectValue = null;
     } else if (this.onPageChange) {
       attrs["command"] = "changePage";
@@ -353,7 +199,7 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     } else if (this.onRefreshValue){
       attrs["command"] = "refreshValue";
       this.onRefreshValue = false;
-      this.resetValueChanged();
+      this._resetValueChanged();
     } else if (this.onBlurValue != null) {
       //console.log("on blur - simply refresh");
     } else {
@@ -363,20 +209,16 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     return attrs;
   };
 
-  override onChangeEvent() {
-    if (this.onChange) {
-    }
-  }
+  public override onChangeEvent() {  }
 
-  public onBlurValue: string
-  onBlurEvent() {
+  public onBlurEvent() {
     this.onBlurValue = this.rawValue;
 
     setTimeout(() => {
       if (this.onBlurValue != null) {
         const needRefresh =  this.tableIsVisible || this.valueHasBeenChanged;
         this.tableIsVisible = false;
-        this.resetValueChanged()
+        this._resetValueChanged()
         if (needRefresh) {
           if (this.rows.length == 1) {
             this.onSelectedEvent(0);
@@ -397,19 +239,20 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
 
   }
 
-  public onSelectValue: number
-  onSelectedEvent(i: number) {
+
+  public onSelectedEvent(i: number, myDrop?: NgbDropdown): void {
+    if (myDrop) {
+      myDrop.close();
+    }
+
     this.onSelectValue = i;
     this.tableIsVisible = false;
     this.refreshPresentedValue();
   }
 
-  public onPageValue: number
-  public onPageChange: string;
-
-  onSwitchSearchModeEvent() {
+  onSwitchSearchModeEvent(myDrop?: NgbDropdown) {
     if (this.tableIsVisible) {
-      this.onCancelSearch();
+      this.onCancelSearch(myDrop);
     } else {
       this.tableIsVisible = true;
       this.onSearchValue = this.rawValue || "";
@@ -427,20 +270,25 @@ export class DictionaryLookupComponent extends FhngInputWithListC implements OnI
     this.fireEventWithLock('onChange', this.onChange);
   }
 
-  onCancelSearch(){
+  public onCancelSearch(myDrop?:NgbDropdown): void {
     this.tableIsVisible = false;
+
+    if (myDrop) {
+      myDrop.close();
+    }
+
     if (this.valueHasBeenChanged){
       this.onRefreshValue = true;
       this.refreshPresentedValue();
     }
   }
 
-  refreshPresentedValue(){
+  public refreshPresentedValue(): void{
     this.fireEventWithLock('onChange', this.onChange);
-    this.resetValueChanged();
+    this._resetValueChanged();
   }
 
-  private resetValueChanged() {
+  private _resetValueChanged(): void {
     this.valueHasBeenChanged = false;
   }
 }
