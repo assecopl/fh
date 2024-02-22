f7d26264bc07fab5b226ec3fb5d727f9db18ce73
Jacek Borowiec <jacek.borowiec@asseco.pl>
2024-02-22 14:06:57 +0100

    [feature]: added possibility to get document version by dbID.

--- fhdp/fhdp-commons/fhdp-commons-transport/src/main/java/pl/fhframework/dp/transport/drs/repository/GetDocumentVersionRequest.java
+++ fhdp/fhdp-commons/fhdp-commons-transport/src/main/java/pl/fhframework/dp/transport/drs/repository/GetDocumentVersionRequest.java
@@ -1,17 +1,11 @@
 package pl.fhframework.dp.transport.drs.repository;
 
+import lombok.Getter;
+import lombok.Setter;
 import pl.fhframework.dp.transport.drs.BaseRequest;
 
+@Getter @Setter
 public class GetDocumentVersionRequest extends BaseRequest {
 	protected String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	
+	protected Long dbId;
 }
