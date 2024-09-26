bd3fdf6366c0b9f9033711506f22edffedb058c1
jacek.borowiec <jacek.borowiec@asseco.pl>
2024-09-26 11:28:04 +0200

    [fix] overcome wrong language parameter in StringPropertyFormatter

--- fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/formatters/StringPropertyFormatter.java
+++ fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/formatters/StringPropertyFormatter.java
@@ -1,10 +1,12 @@
 package pl.fhframework.dp.commons.fh.formatters;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.Formatter;
-import pl.fhframework.dp.commons.utils.lng.LngDescriptionUtil;
 import pl.fhframework.SessionManager;
+import pl.fhframework.UserSession;
 import pl.fhframework.core.i18n.MessageService;
+import pl.fhframework.dp.commons.utils.lng.LngDescriptionUtil;
 import pl.fhframework.format.FhFormatter;
 
 import java.text.ParseException;
@@ -23,6 +25,11 @@ import java.util.regex.Pattern;
 public class StringPropertyFormatter implements Formatter<String> {
     @Autowired
     MessageService messageService;
+
+    @Value("${fhframework.language.viewRawKeyMode:false}")
+    private Boolean viewRawKeyMode = false;
+    @Value("${fhframework.language.default:pl}")
+    private String defaultLanguage;
 
     @Override
     public String parse(String s, Locale locale) throws ParseException {
@@ -43,8 +50,9 @@ public class StringPropertyFormatter implements Formatter<String> {
                     finalParams.add(resolve(p, locale));
                 }
                 String msgKey = elements[0].replace("$.", "");
+                // Error in fh: locale parameter is always pl_PL
                 msg = messageService.getAllBundles().getMessage(msgKey,
-                        finalParams.toArray(new String[0]), locale, "?:" + msgKey);
+                        finalParams.toArray(new String[0]), getUserLanguage(), "?:" + msgKey);
             }
             return msg;
         } else if(key.startsWith("enum.")) {
@@ -62,6 +70,29 @@ public class StringPropertyFormatter implements Formatter<String> {
         } else {
             return p;
         }
+    }
+
+    private Locale getUserLanguage() {
+        UserSession session = SessionManager.getUserSession();
+        if (session != null) {
+            return session.getLanguage();
+        } else {
+            return getLanguageOrDefault(null);
+        }
+    }
+
+    private Locale getLanguageOrDefault(Locale locale) {
+        if (locale == null) {
+            return Locale.forLanguageTag(defaultLanguage);
+        }
+        return locale;
+    }
+
+    private String getDefaultMessage(String key) {
+        if(viewRawKeyMode || getUserLanguage() != null && getUserLanguage().getLanguage() == MessageService.LOCALE_VIEWKEY){
+            return key;
+        }
+        return "Key: " + key + " not found";
     }
 
 }
