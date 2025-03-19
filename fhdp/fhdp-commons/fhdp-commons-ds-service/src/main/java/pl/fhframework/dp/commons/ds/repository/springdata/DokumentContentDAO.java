03899d192732f3394eac2d4e853f87603c5f23c7
Piotr Tajduś <dus@ptdell64.asseco.pl>
2025-03-19 10:27:45 +0100

    fix/chunking big content during update in repository

--- fhdp/fhdp-commons/fhdp-commons-ds-service/src/main/java/pl/fhframework/dp/commons/ds/repository/springdata/DokumentContentDAO.java
+++ fhdp/fhdp-commons/fhdp-commons-ds-service/src/main/java/pl/fhframework/dp/commons/ds/repository/springdata/DokumentContentDAO.java
@@ -2,17 +2,25 @@ package pl.fhframework.dp.commons.ds.repository.springdata;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.BulkOperations;
+import org.springframework.data.mongodb.core.BulkOperations.BulkMode;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.ClientSession;
 
@@ -22,6 +30,7 @@ import pl.fhframework.dp.commons.ds.repository.mongo.model.DocumentContent;
 @Component
 public class DokumentContentDAO extends BaseDAO<DocumentContent> {
 
+//	protected int CHUNK_SIZE = 15000000;
 	protected int CHUNK_SIZE = 15000000;
 	
 	
@@ -107,6 +116,71 @@ public class DokumentContentDAO extends BaseDAO<DocumentContent> {
     	mongoTemplate.save(dbObject, getCollectionName());
     }
 	
+    protected void deleteChunks(String objectID) throws JsonParseException {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("contentId").is(objectID));
+        mongoTemplate.findAllAndRemove(query, getChunkedCollectionName());
+    }   	
+	
+	@Override
+	public void replaceObject(DocumentContent item) throws JsonProcessingException {
+		
+		deleteChunks(item.getId());
+    	
+		BasicDBObject dbObject = new BasicDBObject();
+		dbObject.put("_id", item.getId());
+		
+		byte[] content = item.getContent();
+		
+		dbObject.put("size", content.length);
+		//kompresja
+		content = item.getContent();
+
+		if(content.length>CHUNK_SIZE) {
+			dbObject.put("chunked", true);
+			BasicDBObject chunks = new BasicDBObject();
+			
+			int chunksize = CHUNK_SIZE;
+			
+		    int start = 0;
+		    int chunkNr = 1;
+
+		    List<BasicDBObject> chunksToInsert = new ArrayList<BasicDBObject>();
+		    while (start < content.length) {
+		        int end = Math.min(content.length, start + chunksize);
+		        byte[] chunkContent = Arrays.copyOfRange(content, start, end);
+		        
+		        String chunkID = UUID.randomUUID().toString();
+		        BasicDBObject chunk = new BasicDBObject();
+		        chunk.put("_id", chunkID);
+		        chunk.put("chunkNr", chunkNr);
+		        chunk.put("contentId", dbObject.getString("_id"));
+				Binary cbinary = new Binary(chunkContent);
+		        chunk.put("content", cbinary);
+		        chunks.put("c"+chunkNr, chunkID);
+//		    	mongoTemplate.save(chunk, getChunkedCollectionName());
+//		    	mongoTemplate.insert(chunk, getChunkedCollectionName());
+		    	chunksToInsert.add(chunk);
+		        
+		        start += chunksize;
+		        chunkNr++;
+		        
+		    }
+		    BulkOperations bulkOperations =
+	                mongoTemplate.bulkOps(BulkMode.UNORDERED, getChunkedCollectionName()).insert(chunksToInsert);
+
+		    bulkOperations.execute();
+		    
+	        dbObject.put("chunks", chunks);
+			
+		} else {
+			Binary binary = new Binary(content);
+			dbObject.put("content", binary);
+		}    	
+    	
+    	mongoTemplate.save(dbObject, getCollectionName());
+	}
+	
 	@Override
     public DocumentContent getObject(String objectID) throws IOException {
     	String id = objectID;
@@ -160,6 +234,8 @@ public class DokumentContentDAO extends BaseDAO<DocumentContent> {
         
         
     }   
+	
+
 	
     
 }
